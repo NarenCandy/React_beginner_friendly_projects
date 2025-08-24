3f38fa28fe783c460641d566da5adf058cf1d365
NarenCandy <narencrazy007@gmail.com>
2025-08-24 14:13:01 +0530

    Calculator Project

---
+++ calculator/App.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import Meal from './components/Meal'
+import Calculator from './components/Calculator'
+
+
+
+
+
+const App =() =>{
+  
+  return (
+    <div>
+      <Calculator />
+    </div>
+  )
+  
+}
+
+ export default App
